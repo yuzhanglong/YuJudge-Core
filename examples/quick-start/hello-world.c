aadf75512a45d3fdb4983395561073f7cfd42fad
yuzhanglong <loveyzl1123@gmail.com>
2022-04-23 23:47:24 +0800

    feat: add build script

---
+++ examples/quick-start/hello-world.c
@@ -0,0 +1,14 @@
+/*
+ * C 语言的 hello world 项目
+ * 程序接受用户输入一个整数，
+ * 程序只有一行输出，为 'hello world' 字符串
+ */
+
+#include<stdio.h>
+
+int main(void) {
+    int a;
+    scanf("%d", &a);
+    printf("hello world\n");
+    return 0;
+}
